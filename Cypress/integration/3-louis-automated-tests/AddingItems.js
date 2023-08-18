da19311fb110dfa9d414b96563e0e10c49401b31
louiso109 <louis.odinot@aspeq.com>
2023-08-18 18:02:59 +1200

    adding items automated test

--- Cypress/integration/3-louis-automated-tests/AddingItems.js
+++ Cypress/integration/3-louis-automated-tests/AddingItems.js
@@ -1,5 +1,12 @@
 /// <reference types="cypress" />
 
 it('should be able to add a new item to the list', () => {
-    cy.visit('http://todomvc-app-for-testing.surge.sh')
+
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+
+    cy.get('.new-todo', {timeout: 3000}).type("Clean room{enter}")
+
+    cy.get('.toggle').click()
+
+    cy.contains('clear completed').click()
 })
