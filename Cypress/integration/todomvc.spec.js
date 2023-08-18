75c7050d7831999476b9bfbe9aa346a2419383e1
louiso109 <louis.odinot@aspeq.com>
2023-08-18 14:03:42 +1200

    Updated files/folders

--- Cypress/integration/todomvc.spec.js
+++ Cypress/integration/todomvc.spec.js
@@ -1,5 +1,5 @@
 /// <reference types="cypress" />
 
 it('should navigate to the todomvc app', () => {
-    CSSSkewY.visit('http://todomvc-app-for-testing.surge.sh/')
+    cy.visit('http://todomvc-app-for-testing.surge.sh')
 })
