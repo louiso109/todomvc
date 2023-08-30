d561daede48493f4834215aa4d6f0fbf2d81a99b
louiso109 <louis.odinot@aspeq.com>
2023-08-30 15:43:35 +1200

    complete applitools test

--- Cypress/integration/3-louis-automated-tests/Visual-Validation-Test-Applitools.js
+++ Cypress/integration/3-louis-automated-tests/Visual-Validation-Test-Applitools.js
@@ -2,7 +2,19 @@
 import *  as todoPage from "c:/Development/TodoMVC/cypress/Page-Objects/TodoPage";
 
 describe('visual validation', () => {
+    //before(() => cy.visit("http://todomvc-app-for-testing.surge.sh/"))
     before(() => todoPage.navigate())
+
+    beforeEach(() => cy.eyesOpen({
+        appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
+        browser: [
+            {name: 'chrome', width: 1024, height: 768},
+            {name: 'chrome', width: 800, height: 600},
+            {name: 'firefox', width: 1024, height: 768},
+            {deviceName: 'iPhone X'},
+        ]
+    }))
+    afterEach(() => cy.eyesClose())
 
     it('should look good', () => {
         cy.eyesCheckWindow('empty todo list')
@@ -13,5 +25,7 @@ describe('visual validation', () => {
         cy.eyesCheckWindow('two todos')
 
         todoPage.toggleTodo(0)
+
+        cy.eyesCheckWindow('mark as completed')
     })
 })
